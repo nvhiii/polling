6cb4b26c808451c030ad6c6ccd0e4f782698bc99
nvhiii <nahikhan16@gmail.com>
2025-02-04 18:24:54 -0500

    smart contract + local tests working

--- anchor/tests/basic.spec.ts
+++ anchor/tests/basic.spec.ts
@@ -11,16 +11,33 @@ const votingAddress = new PublicKey("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNN
 describe('Voting', () => {
   // Configure the client to use the local cluster.
 
-  it('Initialize Poll', async () => {
-    
-    const context = await startAnchor("", [{name: "voting", programId: votingAddress}], []);
+  let context;
+  let provider;
+  let votingProgram: Program<Voting>; // need to define type or else we get any type error
 
-	  const provider = new BankrunProvider(context);
+  beforeAll(async () => {
 
-    const votingProgram = new Program<Voting>(
+    context = await startAnchor("", [{name: "voting", programId: votingAddress}], []);
+
+	  provider = new BankrunProvider(context);
+
+    votingProgram = new Program<Voting>(
       IDL,
       provider,
     );
+    
+  })
+
+  it('Initialize Poll', async () => {
+    
+    // context = await startAnchor("", [{name: "voting", programId: votingAddress}], []);
+
+	  // provider = new BankrunProvider(context);
+
+    // votingProgram = new Program<Voting>(
+    //   IDL,
+    //   provider,
+    // );
 
     // instruction handler for init from lib.rs
     await votingProgram.methods.initializePoll(
@@ -55,10 +72,60 @@ describe('Voting', () => {
   // it = init test
   it("Initialize candidate", async () => {
     
+    // we setting both candidates here for the poll
+    // when calling this method in the test, we need to pass both poll id and candidate name as params
+    // needs to be the same param order as the instruction method
+    await votingProgram.methods.initializeCandidate(
+      "Smooth",
+      new anchor.BN(1),
+    ).rpc();
+
+    await votingProgram.methods.initializeCandidate(
+      "Crunchy",
+      new anchor.BN(1),
+    ).rpc();
+
+    // when setting up acc, make sure params are in the same order as the contract in lib.rs
+    // poll address must be in the same format as before in the test specified as well
+    const [crunchyAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Crunchy")],
+      votingAddress,
+    );
+
+    const crunchyCandidate = await votingProgram.account.candidate.fetch(crunchyAddress);
+    console.log(crunchyCandidate);
+    // do a test to check initialized values, if they are tested and are what we expect
+    expect(crunchyCandidate.candidateVotes.toNumber()).toEqual(0);
+
+    const [smoothAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Smooth")],
+      votingAddress,
+    );
+
+    const smoothCandidate = await votingProgram.account.candidate.fetch(smoothAddress);
+    console.log(smoothCandidate);
+    expect(smoothCandidate.candidateVotes.toNumber()).toEqual(0);
+
   })
 
-  it("Initialize candidate", async () => {
+  it("Vote", async () => {
     
+    await votingProgram.methods.vote(
+
+      "Smooth",
+      new anchor.BN(1),
+
+    ).rpc();
+
+    const [smoothAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Smooth")],
+      votingAddress,
+    );
+
+    const smoothCandidate = await votingProgram.account.candidate.fetch(smoothAddress);
+    console.log(smoothCandidate);
+    expect(smoothCandidate.candidateVotes.toNumber()).toEqual(1);
+
   })
   
 });
